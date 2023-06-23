ff7cdec2f683c253fee2636ac023f646d06e586e
Nkatha_Glory <kinyua6945@gmail.com>
2023-06-23 09:20:11 +0300

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Restaurant.create(name: 'Sottocasa NYC', location: '298 Atlantic Ave, Brooklyn, NY 11201')
+Restaurant.create(name: 'PizzArte', location: '69 W 55th St, New York, NY 10019')
+
+Pizza.create(name: 'Hawaaian', ingredients: 'Dough, Cooked Bacon, ham')
+Pizza.create(name: 'Beef', ingredients: 'Dough, Tomato Sauce, Cheese, Beef')
+
+RestaurantPizza.create(restaurant_id: 1, pizza_id: 1, price: 10)
+RestaurantPizza.create(restaurant_id: 1, pizza_id: 2, price: 12)
